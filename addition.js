5d1d903b8c88921f80b1dd04bd1368376eff578c
Lalithaguntreddy <147490667+Lalithaguntreddy@users.noreply.github.com>
2023-10-12 15:37:59 +0530

    Create addition.js

---
+++ addition.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>lally</title>
+ <body>
+    <h1>lally</h1>
+    <script>
+        var x=4;
+        x+=6;
+        console.log(x);
+    </script>
+ </body>
+</head>
+</html>
